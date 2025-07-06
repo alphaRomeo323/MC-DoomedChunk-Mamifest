63da4a475e8095584a3552e7e0e6b24adb429d39
alphaRomeo323 <58612683+alphaRomeo323@users.noreply.github.com>
2025-07-06 21:00:04 +0900

    Update entrypoint.go

--- entrypoint/entrypoint.go
+++ entrypoint/entrypoint.go
@@ -9,10 +9,29 @@ import (
 	"syscall"
 )
 
+const prepare = true
 const backup = false
 
 func main() {
+	//prepare.sh実行
+	if prepare {
+		preparecmd := exec.Command("./backup.sh")
+		preparecmd.Stdin = os.Stdin
+		preparecmd.Stdout = os.Stdout
+		preparecmd.Stderr = os.Stderr
+		err = preparecmd.Run()
+		if err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				log.Printf("prepare.sh returned non-zero code: %v", err)
+			} else {
+				log.Printf("Failed to run prepare.sh: %v", err)
+				os.Exit(2)
+			}
+		}
+	}
+	//javaコマンド定義
 	startcmd := exec.Command("java", "@user_jvm_args.txt", "@libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt", "nogui", "\"$@\"")
+	//パイプライン準備
 	stdinpipe, err := startcmd.StdinPipe()
 	if err != nil {
 		log.Fatalf("Failed to set stdinpipe: %v", err)
@@ -20,6 +39,7 @@ func main() {
 	go io.Copy(stdinpipe, os.Stdin)
 	startcmd.Stdout = os.Stdout
 	startcmd.Stderr = os.Stderr
+	//スタート
 	err = startcmd.Start()
 	if err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
@@ -29,6 +49,7 @@ func main() {
 			log.Fatalf("Server failed during init: %v", err)
 		}
 	}
+	//初期化監視
 	waitCh := make(chan int)
 	go func() {
 		err = startcmd.Wait()
@@ -44,6 +65,7 @@ func main() {
 			waitCh <- 0
 		}
 	}()
+	//シグナル監視
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGTERM)
 	exitCode := -1
